8456f2f05f68392af77a5ebb8f2a1adfe5c5479b
Franco <francorr240@gmail.com>
2023-08-20 15:07:08 -0300

    Task 7 complete

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from os.path import exists
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
@@ -8,15 +16,24 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
+    classes = {
+        'BaseModel': BaseModel, 'User': User, 'Place': Place,
+        'State': State, 'City': City, 'Amenity': Amenity,
+        'Review': Review
+    }
+
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls:
-            objs = {}
-            for key, value in FileStorage.__objects.items():
-                if key.split(".")[0] == cls.__name__:
-                    objs[key] = value
-            return objs
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        return {key: value for key, value
+                in FileStorage.__objects.items() if isinstance(value, cls)}
+
+    def delete(self, obj=None):
+        """Delete obj from __objects dict"""
+        if obj:
+            key_to_del = f"{str(obj.__class__.__name__)}.{obj.id}"
+            FileStorage.__objects.pop(key_to_del)
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -32,33 +49,15 @@ class FileStorage:
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
+        """ Deserializes the JSON file to __objects """
+        if exists(self.__file_path):
+            with open(FileStorage.__file_path) as file:
+                data = json.load(file)
+            dummy = None
+            for key, value in data.items():
+                dummy = self.classes[key.split('.')[0]](**value)
+                FileStorage.__objects[key] = dummy
 
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
-        try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
-        except FileNotFoundError:
-            pass
-
-    def delete(self, obj=None):
-        """Deletes an object"""
-        if obj:
-            key = f"{obj.__class__.__name__}.{obj.id}"
-            if key in FileStorage.__objects:
-                del FileStorage.__objects[key]
-                self.save()
+    def close(self):
+        """reload"""
+        self.reload()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """State class"""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref="state",
+                              cascade="all, delete, delete-orphan")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """Getter"""
+            from models import storage
+            all_cities = []
+            for key, value in storage._FileStorage__objects.items():
+                key = key.split(".")
+                if key == 'City' and value.state_id == self.id:
+                    all_cities.append(value)
+            return all_cities
